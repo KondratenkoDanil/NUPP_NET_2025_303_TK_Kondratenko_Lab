42d3f3cdb7fb2d70f72abd7d365ba37ff7c45f74
KondratenkoDanil <129153766+KondratenkoDanil@users.noreply.github.com>
2025-05-30 00:57:50 +0300

    Delete Lab3 directory

--- Lab3/ConsoleApp2/Transport.Infrastructure/Models/RouteModel.cs
+++
@@ -1,8 +0,0 @@
-﻿public class RouteModel
-{
-    public int Id { get; set; }
-    public string Destination { get; set; } = string.Empty;
-
-    public int BusId { get; set; }
-    public BusModel Bus { get; set; } = null!;
-}
